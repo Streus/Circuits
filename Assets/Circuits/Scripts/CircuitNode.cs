eae20346bd64715bf6489e6a651415079bb940c4
Sam Streed <embersteelstreus@gmail.com>
2019-02-19 13:17:39 -0600

    Base CircuitNode impl set up.

---
+++ Assets/Circuits/Scripts/CircuitNode.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Circuits
+{
+	[ExecuteInEditMode, DisallowMultipleComponent]
+	public abstract class CircuitNode : MonoBehaviour
+	{
+		public bool Activated { get { return activated; } private set { activated = value; } }
+		[SerializeField]
+		private bool activated = true;
+
+		private bool nextActivatedValue;
+
+		protected abstract bool EvalState();
+
+		private void Update()
+		{
+			nextActivatedValue = EvalState();
+		}
+
+		private void LateUpdate()
+		{
+			Activated = nextActivatedValue;
+		}
+
+#if UNITY_EDITOR
+		protected virtual void OnDrawGizmos()
+		{
+			Gizmos.color = Activated ? Color.green : Color.gray;
+			Gizmos.DrawWireSphere(transform.position, 1f);
+		}
+#endif
+	}
+}
---
+++ Assets/Circuits/Scripts/Relay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Circuits
+{
+	public class Relay : CircuitNode
+	{
+		[SerializeField]
+		private CircuitNode node;
+
+		protected override bool EvalState()
+		{
+			if (node != null)
+				return node.Activated;
+			else
+				return Activated;
+		}
+	}
+}
